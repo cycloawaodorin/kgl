fb11a0ad1463de00c6a06d50e6db83b07dedefd3
cycloawaodorin <cycloawaodorin+github@gmail.com>
2019-08-31 01:29:39 +0900

    add Integer#to_msm

--- lib/kgl.rb
+++ lib/kgl.rb
@@ -1,3 +1,12 @@
 require "kgl/version"
 require "kgl/suu"
 require "kgl/kmath"
+
+class Integer
+	def to_msm(fps=60)
+		s, f = divmod(fps)
+		ms = (f*1000).quo(fps).round
+		m, s = s.divmod(60)
+		%Q|#{m}:#{s}.#{ms}|
+	end
+end
